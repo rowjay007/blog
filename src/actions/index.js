976cdaff9d59a30dacc6ceb23a87eea1bf8211aa
Rowland Adimoha <rowlives@gmail.com>
2020-11-04 10:18:41 +0100

    implemented redux-thunk into our app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,9 +1,10 @@
 import jsonPlaceholder from "../api/jsonPlaceholder";
 
-export const fetchPosts = () => {
-  const promise = jsonPlaceholder.get("./posts");
-  return {
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("./posts");
+
+  dispatch({
     type: "FETCH_POSTS",
-    payload: promise,
-  };
+    payload: response,
+  });
 };
--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from "react-dom";
 import App from "./components/App";
 import reducers from "./reducers";
 import { Provider } from "react-redux";
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+
+const store = createStore(reducers, applyMiddleware(thunk));
 
 ReactDOM.render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.querySelector("#root")
